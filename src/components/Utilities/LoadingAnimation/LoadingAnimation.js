496303e1c8d4bc027f9e1cd6a8032984ffce4d2a
Cameron DeCoster <cameron.decoster@gmail.com>
2020-07-08 05:53:32 -0600

    added correct favicon this time

--- src/components/Utilities/LoadingAnimation/LoadingAnimation.js
+++ src/components/Utilities/LoadingAnimation/LoadingAnimation.js
@@ -1,12 +1,13 @@
 // React
 import React from "react";
 
-// Configuratoin
+// Configuration
 import "./LoadingAnimation.css";
 
 export default function LoadingAnimation() {
     return (
         <div id='container_spinner'>
+            {/* Used under CC0 license from https://loading.io/css/ */}
             <div className='lds-spinner'>
                 <div></div>
                 <div></div>
